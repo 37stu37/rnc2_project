a2b8cbbc83c3d2f075a3710a2f853efee2a29760
37stu37 <37stu37@gmail.com>
2022-02-16 12:13:42 +0000

    Update prototype_graph.jl

--- prototype_graph.jl
+++ prototype_graph.jl
@@ -14,97 +14,49 @@ begin
     "river9", "infrastructure", "infrastructure", "river1", "river2"],
     type_source= ["river", "river", "river", "river", "river",
     "dam", "river", "river", "river", "river",
-    "landslide", "landslide", "river", "catchment", "catchment"],
-    type_target= ["river", "river", "river", "river",
-    "dam", "river", "river", "river", "river", "river1",
-    "river", "infrastructure", "infrastructure", "river", "river"],
-    source_ID = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 11, 9, 13, 14],
-    target_ID = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 10, 12, 12, 1, 2],
-    edge_order = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0, 0, 0, 0, 0])
+    "landslide", "landslide", "river", "catchment", "catchment"])
+    # type_target= ["river", "river", "river", "river",
+    # "dam", "river", "river", "river", "river", "river1",
+    # "river", "infrastructure", "infrastructure", "river", "river"],
+    # source_ID = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 11, 9, 13, 14],
+    # target_ID = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 10, 12, 12, 1, 2],
+    # edge_order = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0, 0, 0, 0, 0])
 
-    @. df.magnitude = 1000 # base discharge
+    @. df.magnitude = ifelse((df.type_source == "river") | (df.type_source == "dam"), 1000.0, 0.0) # base discharge
+    @. df.delay = 1
+    @. df.time = 0
 end
 
-# add base flow to river nodes
-df.magnitude +=1000
-# Create random temporal events
-landslide(x,y) = (x=="landslide") ? rand(Pareto(α, θ), 1)[1] : y
-df.magnitude = landslide.(df.type_source, df.magnitude)
-runoff(x,y)= (x=="catchment") ? rand(Pareto(α, θ), 1)[1] : y
-df.magnitude = runoff.(df.type_source, df.magnitude)
 
+clock = []
+futures = []
+push!(clock, df)
 
-# Create the next dataframe
-next = df[:, [:target, :magnitude]]
-d = outerjoin(df[:, Not(:magnitude)], next, on=[:source => :target], makeunique=true)
+for time in 1:10
+    global last_df = clock[end] # last_df state of the network
+    
+    # update original network to be updated to current state
+    @. last_df.time = time
 
+    # Create random temporal events (here landslide volume and runoff) during the time interval if delay of the previous event is passed
+    @. last_df.magnitude = ifelse(((last_df.type_source == "landslide") | (last_df.type_source == "catchment")) & (last_df.delay == time), rand(Pareto(3, 1000)), last_df.magnitude)
 
+    # and reset delay depending on type (assuming runoff takes 2 time intervals) if passed
+    @. last_df.delay = ifelse((last_df.type_source == "catchment") & (last_df.delay == time), time + 2, last_df.delay)
 
-# input
-begin
-    # edge_order would be DEM value
-    df = DataFrame(source = ["river1", "river2", "river3", "river4", "river5",
-    "dam", "river6", "river7", "river8", "river9",
-    "landslide", "landslide", "river8", "catchment1", "catchment2"], 
-    target = ["river2", "river3", "river4", "river5",
-    "dam", "river6", "river7", "river8", "river9", "river10",
-    "river9", "infrastructure", "infrastructure", "river1", "river2"],
-    type_source= ["river", "river", "river", "river", "river",
-    "dam", "river", "river", "river", "river",
-    "landslide", "landslide", "river", "catchment", "catchment"],
-    type_target= ["river", "river", "river", "river",
-    "dam", "river", "river", "river", "river", "river1",
-    "river", "infrastructure", "infrastructure", "river", "river"],
-    source_ID = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 11, 9, 13, 14],
-    target_ID = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 10, 12, 12, 1, 2],
-    edge_order = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0, 0, 0, 0, 0])
+    # keep currently activating edge list & delayed node to the next time interval
+    global delayed = filter([:delay, :time] => (x,y) -> x>y, last_df) # keep delayed edges
+    delayed = delayed[:, [:source, :target, :type_source, :magnitude, :delay, :time]]
+    global current = filter([:delay, :time] => (x,y) -> x<=y, last_df) # keep active edges
+    current = current[:, [:source, :target, :type_source, :magnitude, :delay, :time]]
 
-    # input parameters
-    df_clock = []
-    river_base_flow = 1000.0
+    current = vcat(current, delayed)
+    
+    # move active edges forward (target becomes source in the orginal dataset) based on their type
+    @. current.source = ifelse((current.type_source == "river") | (current.type_source == "dam"), current.target, current.source)
+    current = current[:, [:source, :type_source, :magnitude, :delay, :time]]
+    
+    # NEED TO GO BACK TO THE INITIAL NETWORK SETUP BUT WITH THE UPDATED MAGNITUDE DELAY TIME
 
-    # Add the discharge
-    df.magnitude .= river_base_flow
-
-    # Parameters for pareto distributions
-    α = 3
-    θ = 100
-
-    # create delay attribute for each edges
-    df.delay .= 0
-    df.time .= 0
-
-    # state @ t0
-    push!(df_clock, df)
+    push!(clock, current)
 end
-
-# algorithm
-time_max = 2
-
-# for t in 1:time_max
-
-# start from the last state
-d = df_clock[end]
-
-# Random events that can happened randomly at each time
-# Create artificial runoff for each catchments (should come from weather pattern and volcanic activity)
-runoff(x) = (x=="catchment") ? rand(Pareto(α, θ), 1)[1] : 0
-rof = runoff.(df.type)
-
-# Create artificial landslide for each landslide source
-landslide(x,y) = (x=="landslide") ? rand(Pareto(α, θ), 1)[1] : y
-ls = landslide.(df.type_source, df.target)
-
-# Add magnitude to edges
-d.magnitude = rof .+ ls
-
-# propagate flow downstream
-next_df = select(d, Not(:source))
-d = innerjoin(d, next_df, on=[:source => :target], makeunique=true)
-
-# record event at time t and "remove" delay
-d.time .= t
-d.delay .-= 1
-push!(df_clock, d)
-
-# end
