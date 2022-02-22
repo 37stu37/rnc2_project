27c30c372162ce68d69a04eb641b5525beaf07e9
37stu37 <37stu37@gmail.com>
2022-02-22 16:58:25 +0000

    u

--- prototype_graph.jl
+++ prototype_graph.jl
@@ -5,38 +5,15 @@ using Graphs
 using GraphPlot
 using DataFrames, Random, Distributions, ProgressBars, Chain
 
-
-begin 
-    # set up input parameters
-    network = DataFrame(source = ["river1", "river2", "river3", "river4", "river5",
-        "dam", "river6", "river7", "river8", "river9",
-        "landslide", "landslide", "river8", "catchment1", "catchment2"], 
-        target = ["river2", "river3", "river4", "river5",
-        "dam", "river6", "river7", "river8", "river9", "river10",
-        "river9", "infrastructure", "infrastructure", "river1", "river2"],
-        type_source= ["river", "river", "river", "river", "river",
-        "dam", "river", "river", "river", "river",
-        "landslide", "landslide", "river", "catchment", "catchment"],
-        magnitude = [1001.0, 1002.0, 1003.0, 1004.0, 1005.0, 1006.0, 1007.0, 1008.0, 1009.0, 1010.0, 0.0, 0.0, 1009.0, 0.0, 0.0])
-
-    global_time = 1
-    @. network.time = global_time
-    @. network.global_time = global_time
-
-    clock = []
-    push!(clock, network)
-end
-
-
+# functions
 begin
-    # functions
     function river_and_dams(dataframe)
         df = filter(:type_source => x -> (x=="river")||(x=="dam"), dataframe)
         df = innerjoin(df[:, Not([:magnitude, :time])], 
                        df[:, [:target, :magnitude, :time]], 
                        on=:source=>:target)
         @. df.time += 1
-        println("max river time : $(maximum(df.time))")
+        
         push!(present, df)
     end
 
@@ -48,7 +25,7 @@ begin
         @. df.magnitude = ifelse(df.time == df.global_time, rand(Pareto(3, 1000)), df.magnitude)
         # reset delay to 3 time units if time has come for a reset
         @. df.time = ifelse(df.time == df.global_time, df.global_time + 3, df.time)
-        println("max landslide time : $(maximum(df.time))")
+        
         push!(present, df)
     end
 
@@ -73,7 +50,7 @@ begin
                             df.magnitude_1, 
                             100) # could be updated to match other nodes
         @. df.time += 1
-        println("max catchment time : $(maximum(df.time))")
+        
         push!(present, df[:, [:source, :target, :type_source, :magnitude, :time, :global_time]])
     end
 
@@ -87,10 +64,33 @@ begin
 end
 
 
-# Algorithm #################################
-for global_time in tqdm(1:6)
+# set up
+begin 
+    # set up input parameters
+    network = DataFrame(source = ["river1", "river2", "river3", "river4", "river5",
+        "dam", "river6", "river7", "river8", "river9",
+        "landslide", "landslide", "river8", "catchment1", "catchment2"], 
+        target = ["river2", "river3", "river4", "river5",
+        "dam", "river6", "river7", "river8", "river9", "river10",
+        "river9", "infrastructure", "infrastructure", "river1", "river2"],
+        type_source= ["river", "river", "river", "river", "river",
+        "dam", "river", "river", "river", "river",
+        "landslide", "landslide", "river", "catchment", "catchment"],
+        magnitude = [1001.0, 1002.0, 1003.0, 1004.0, 1005.0, 1006.0, 1007.0, 1008.0, 1009.0, 1010.0, 0.0, 0.0, 1009.0, 0.0, 0.0])
 
-    println("global time : $global_time")
+    global_time = 1
+    @. network.time = global_time
+    @. network.global_time = global_time
+
+    clock = []
+    push!(clock, network)
+end
+
+
+# Algorithm #################################
+for global_time in tqdm(1:100000)
+
+    # println("global time : $global_time")
 
     global present = []
     past = clock[end]
