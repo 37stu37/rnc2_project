f7dcc1a8e182dd734f5eb013d71531283fc31312
37stu37 <37stu37@gmail.com>
2023-03-21 15:55:43 +0000

    updated folder architecture

---
+++ code/mods_trigo.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from numba import njit
+
+@njit
+def square(x):
+	return x ** 2
+
+@njit
+def distance(x, y):
+	return np.sqrt(square(x) + square(y))
+
+
+@njit
+def bearing(x, y):
+	b = np.degrees(np.arctan2(x,y))
+#     degrees = (degrees + 360) % 360
+	return (b + 360) % 360
+
